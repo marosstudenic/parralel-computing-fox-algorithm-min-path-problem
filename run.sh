5183bf21c72895322910f5781de863d85ed610e8
Maros Studenic <studenic.maros@gmail.com>
2023-11-06 18:31:46 +0000

    update test script

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 rm -rf output/*
 
-make && mpirun --allow-run-as-root -np 4 ./fox < tests/inputs/input300 > output/fox.out
+make && mpirun --allow-run-as-root -np 4 ./fox < tests/inputs/input1200 > output/fox.out
+
+
+# test output agains expected output in tests/outputs/output300
+diff output/fox.out tests/outputs/output1200 > /dev/null
+# write ok if no difference
+if [ $? -eq 0 ]
+then
+    echo "nic sa nepokazilo"
+else
+    echo "fuck it"
+fi
