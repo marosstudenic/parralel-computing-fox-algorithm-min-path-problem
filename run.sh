9337da91aa5ff7bc07b3492cf3c9b08d4d66a1af
Maros Studenic <studenic.maros@gmail.com>
2023-11-06 18:50:50 +0000

    test different file instead of stdout

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 rm -rf output/*
 
-make && mpirun --allow-run-as-root -np 4 ./fox < tests/inputs/input1200 > output/fox.out
+make && mpirun --allow-run-as-root -np 4 ./fox < tests/inputs/input300
 
 
 # test output agains expected output in tests/outputs/output300
-diff output/fox.out tests/outputs/output1200 > /dev/null
+diff output/proc-0.out tests/outputs/output300 > /dev/null
 # write ok if no difference
 if [ $? -eq 0 ]
 then
